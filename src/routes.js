dbbe788f8abbd72f0554dfd188b04cf066a2d4b6
Filipe Augusto Gonçalves <flpgst@gmail.com>
2019-10-24 23:19:13 -0300

    Criado upload de arquivos para perfil de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import Multer from 'multer'
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 import AuthMiddleware from './app/middlewares/auth'
+import MulterConfig from './config/multer'
 
 const routes = new Router()
+const upload = Multer(MulterConfig)
 
 routes.get('/users', UserController.index)
 
@@ -13,5 +16,9 @@ routes.post('/sessions', SessionController.store)
 routes.use(AuthMiddleware)
 
 routes.put('/users', UserController.update)
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true })
+})
 
 export default routes
