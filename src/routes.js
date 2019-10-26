9e5d99fe1249344b4abb84b74b4bed786c3cd0ec
Filipe Augusto Gonçalves <flpgst@gmail.com>
2019-10-26 01:22:22 -0300

    Criado Controller para prestadores de serviço

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Multer from 'multer'
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 import FileController from './app/controllers/FileController'
+import ProviderController from './app/controllers/ProviderController'
 
 import AuthMiddleware from './app/middlewares/auth'
 import MulterConfig from './config/multer'
@@ -19,6 +20,8 @@ routes.post('/sessions', SessionController.store)
 routes.use(AuthMiddleware)
 
 routes.put('/users', UserController.update)
+
+routes.get('/providers', ProviderController.index)
 
 routes.post('/files', upload.single('file'), FileController.store)
 
