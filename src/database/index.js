3f785215af9c853722dacee29388d4fba876333a
Filipe Augusto Gonçalves <flpgst@gmail.com>
2019-10-30 22:49:31 -0300

    configurado conexão com mongoDB em localhost

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize'
+import Mongoose from 'mongoose'
 
 import User from '../app/models/User'
 import File from '../app/models/File'
@@ -18,6 +19,13 @@ class Database {
     models.map(model => model.init(this.connection))
     models.map(
       model => model.associate && model.associate(this.connection.models)
+    )
+  }
+
+  mongo() {
+    this.mongoConnection = Mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true }
     )
   }
 }
