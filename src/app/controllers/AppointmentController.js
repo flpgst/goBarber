f5501a5027ff1f4ba1228a1fcdcb095dd1b13737
Filipe Augusto Gonçalves <flpgst@gmail.com>
2019-11-01 02:18:38 -0300

    Adicionado envio de emails com templates

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,7 @@ import User from '../models/User'
 import Appointment from '../models/Appointment'
 import File from '../models/File'
 import Notification from '../schemas/Notification'
+import Mail from '../../lib/Mail'
 
 class AppointmentController {
   async index(req, res) {
@@ -113,7 +114,23 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id)
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email']
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name']
+        }
+      ]
+    })
+
+    if (!appointment)
+      return res.status(401).json({ error: 'Agendamento inexistente' })
 
     if (appointment.user_id !== req.userId) {
       return res
@@ -131,7 +148,24 @@ class AppointmentController {
 
     appointment.canceled_at = new Date()
 
-    appointment.save()
+    await appointment.save()
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento Cancelado',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          parseISO(appointment.date),
+          "'dia' dd 'de' MMMM', às' H:mm'h'",
+          {
+            locale: pt
+          }
+        )
+      }
+    })
 
     return res.json(appointment)
   }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd7ac984a68f16e',
+    pass: 'aafeefd2dad990'
+  },
+  default: {
+    from: 'GoBarber <nãoresponda@gobarber.com.br>'
+  }
+}
